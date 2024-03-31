5f87ddb50688b229488fe3e0b7d76a512b1d082e
garrigueta <ferran.garriga@gmail.com>
2024-03-31 17:16:03 +0200

    Add about and profile routes with handlebars template engine

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,18 +1,27 @@
 #[macro_use] extern crate rocket;
+use rocket_dyn_templates::{Template, context};
 
 #[get("/")]
 fn index() -> &'static str {
     "Index!"
 }
 
-#[get("/")]
-fn world() -> &'static str {
-    "Hello, world!"
+#[get("/about")]
+fn about() -> Template {
+    Template::render("about", context! {
+        title: "About",
+        content: "This is the about page! <br> This page uses rocket and handlebars template engine"
+    })
+}
+
+#[get("/profile")]
+fn profile() -> &'static str {
+    "Profile"
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index])
-    .mount("/world", routes![world])
+    .mount("/", routes![index, about, profile])
+    .attach(Template::fairing())
 }
